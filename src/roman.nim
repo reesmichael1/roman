ac1a4bec26742830a2726eb58a967004cda35100
Michael Rees <mrees@noeontheend.com>
2019-08-30 16:46:38 -0500

    Clarify comment about ValueError

--- src/roman.nim
+++ src/roman.nim
@@ -66,8 +66,8 @@ proc main(url: string) {.raises: [].} =
   except RomanError as e:
     echo "error: ", e.msg
     quit(1)
-  # ValueError is raised by strformat if a bad format string is given to fmt
-  # In theory, this path is unreachable
+  # ValueError is raised by strformat if a bad format string is given to fmt,
+  # so if this patch is reached, it's because of a typo in the source.
   except ValueError:
     echo "undescribable error during execution, please file a bug"
     quit(1)
