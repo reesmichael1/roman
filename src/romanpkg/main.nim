73f8e46274e10ea7106b3b05be6449e6057c046d
Michael Rees <mrees@noeontheend.com>
2019-08-31 13:06:55 -0500

    Ask user to select which feed to see posts from

--- src/romanpkg/main.nim
+++ src/romanpkg/main.nim
@@ -1,17 +1,34 @@
 import os
+import sequtils
+
+import FeedNim / rss
 
 import errors
 import feeds
 import subscriptions
+import termask
 
 
 proc runMainPath() {.raises: [RomanError].} =
   let subs = getSubscriptions()
+  var feed: Rss
   if subs.len == 0:
-    echo "You aren't subscribed to any feeds yet!"
-  for sub in subs:
-    let feed = getFeed(sub)
-    displayFeed(feed)
+    echo "You aren't subscribed to any feeds yet! ",
+      "Use --subscribe [url] to add some."
+    return
+  elif subs.len == 1:
+    feed = getFeed(subs[0])
+  else:
+    let urls = map(subs, proc(s: Subscription): string = s.url)
+    try:
+      let sub = promptList("Select Feed [Tab/Enter]", urls)
+      feed = getFeed(Subscription(url: sub))
+    except ValueError as e:
+      raise newException(RomanError, e.msg)
+    except IOError as e:
+      raise newException(RomanError, e.msg)
+
+  displayFeed(feed)
 
 
 proc addSubscription(url: string) {.raises: [RomanError].} =
