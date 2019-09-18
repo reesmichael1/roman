398fe417378106d7774612c53a30a498d74bd991
Michael Rees <mrees@noeontheend.com>
2019-09-18 17:05:39 -0500

    Add version flag and message

--- src/roman.nim
+++ src/roman.nim
@@ -3,12 +3,20 @@ import argparse
 import romanpkg / main
 
 
+proc showVersion() =
+  echo "roman 0.1.0"
+
+
 when isMainModule:
   var p = newParser("roman"):
     help("a command line RSS feed reader")
     option("-s", "--subscribe", help = "a URL to add to the subscription list")
+    flag("-v", "--version", help = "print version information")
     run:
-      main(subscribeURL = opts.subscribe)
+      if opts.version:
+        showVersion()
+      else:
+        main(subscribeURL = opts.subscribe)
 
   try:
     p.run()
